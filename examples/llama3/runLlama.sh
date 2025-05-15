7acfa6779f856bd2dcb663b248413bb9047f8db9
Alan Chapman <acchapm1@asu.edu>
2025-05-15 15:00:55 -0700

    updated llama3 scripts with new cache paths

--- examples/llama3/runLlama.sh
+++ examples/llama3/runLlama.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 readonly currentDir="$(pwd)"
-readonly dataDir="$currentDir/data"
+readonly dataDir="/scratch/acchapm1/.cache/llama3"
 readonly cacheDir="$dataDir/nim-cache"
 readonly hfDir="$dataDir/huggingface"
-readonly img="$dataDir/llama3.3.sif"
 
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
@@ -14,4 +13,4 @@ apptainer run \
 	--nv \
 	--env NGC_API_KEY=$NGC_API_KEY \
 	--bind $cacheDir:/opt/nim/.cache \
-	$img
+	llama3-nim.sif
